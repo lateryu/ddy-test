ab6837fec853a1394e2d44314b1d54e2a20f3b3e
Dan <yddzling@163.com>
2021-06-17 23:00:48 +0800

    添加husk2

---
+++ xxx.js
@@ -0,0 +1,10 @@
+const foo = (fn1, fn1, fn1, fn1) => {
+  console.log("aaaaa");
+  console.log("aaaaa");
+  console.log("aaaaa");
+  console.log("aaaaa");
+  console.log("aaaaa");
+  console.log("aaaaa");
+  console.log("aaaaa");
+};
+foo();
